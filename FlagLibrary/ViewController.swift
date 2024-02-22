4fcffe16d444d3989a2764f9e1ebbfcaa61f008a
Artsiom Chekh <qbartem@gmail.com>
2024-02-22 17:53:42 +0300

    Initial commit.

---
+++ FlagLibrary/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  FlagLibrary
+//
+//  Created by Artsiom Chekh on 22.02.24.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasSuffix("jpg") {
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        cell.imageView?.image = UIImage(named: pictures[indexPath.row])
+        return cell
+    }
+}
+
